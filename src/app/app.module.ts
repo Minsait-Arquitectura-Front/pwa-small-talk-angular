b04000abc4599ae7d000ec104fd91d6b3d59d183
Fran Linde Blázquez <franlindebl@gmail.com>
2023-05-17 11:16:09 +0200

    Added service workers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { PokemonListComponent } from 'src/components/PokemonList/pokemon-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PokemonItemComponent } from 'src/components/PokemonItem/pokemon-item.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,12 @@ import { PokemonItemComponent } from 'src/components/PokemonItem/pokemon-item.co
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
