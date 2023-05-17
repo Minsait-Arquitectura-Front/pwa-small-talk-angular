d22e70193a60996a4dcf1a7348f382707a0eb520
Fran Linde Blázquez <franlindebl@gmail.com>
2023-05-17 11:13:22 +0200

    Added working sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonListComponent } from 'src/components/PokemonList/pokemon-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonItemComponent } from 'src/components/PokemonItem/pokemon-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonItemComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/components/PokemonList/pokemon-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface PokemonInList {
+  name: string;
+  url: string;
+}
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css'],
+})
+export class PokemonListComponent implements OnInit {
+  public pokemons: PokemonInList[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit() {
+    this.http.get<any>('https://pokeapi.co/api/v2/pokemon?limit=10')
+      .subscribe(response => {
+        this.pokemons = response.results;
+      });
+  }
+}
